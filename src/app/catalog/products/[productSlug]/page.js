06510dfb8e3320a45824bb3d26bc408f841aa135
NickitaStepin <n1ckita.stepin@yandex.com>
2024-10-13 00:35:39 +0300

    Страница продукта

--- src/app/catalog/products/[productSlug]/page.js
+++ src/app/catalog/products/[productSlug]/page.js
@@ -1,7 +1,32 @@
 import React from 'react'
+import './products.scss'
+import Image from "next/image";
+
 
 export default function Page() {
     return (
-        <div>Page</div>
+        <>
+        <div id="products">
+            <div className="mainContainer">
+            <div className="container">
+                <div className="carousel">
+                    <Image src="/products/example.png" width="484" height="489" alt="/" />
+                </div>
+                <div className="text">
+                    <div className="firstLine">
+                    <h1>Черный квадрат</h1>
+                        <Image src="/products/heart.png" className="favorite" width="40" height="40" alt="favorite" />
+                    </div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lectus risus, finibus ornare vestibulum et, feugiat quis dui. Vivamus sit amet dolor et magna facilisis rhoncus. Curabitur maximus est sed porta scelerisque. Sed suscipit, arcu volutpat feugiat posuere, eros nisi tristique nibh, mollis vehicula lectus tortor eu purus. Donec ut tortor blandit, sagittis diam eget, suscipit eros. Quisque at magna neque. Nulla faucibus mi at nunc mattis placerat. Pellentesque quis augue quis elit tristique auctor. Integer varius est orci, vel egestas felis dictum nec. Phasellus porta ex sit amet turpis finibus, sed vestibulum nisl efficitur. Praesent ultrices diam enim. In ut tellus sed sem placerat sollicitudin. Donec quis mollis dolor. Etiam viverra, arcu cursus porttitor porttitor, diam nunc auctor nisl, quis placerat magna erat et odio. Phasellus feugiat turpis quis mollis lacinia. Sed ac orci et nisi venenatis pharetra ac non arcu.</p>
+                <div className="price">
+                    <h1>100₽</h1>
+                    <Image src="/products/Line.png" height="4" width="500" alt="/" />
+                    <Image src="/products/shoppingCart.png" width="67" height="44" alt="shoppingCart" className="shoppingCart" />
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </>
     )
 }
